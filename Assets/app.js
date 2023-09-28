3c5bcc4577526362176d9d283c2d5e91fec68afa
Kimpes <gustafssonfilip@hotmail.com>
2023-09-28 12:38:36 +0200

    Created framework for the hand
    It uses the mouse as a placeholder

--- Assets/app.js
+++ Assets/app.js
@@ -1,8 +1,32 @@
-function setup() {
-    createCanvas(innerWidth, innerHeight);
-    background(34, 39, 46);
-  }
+//Global Variables
+let mouse;
 
-  function draw() {
-    ellipse(50, 50, 50);
+//Setup
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background(34, 39, 46);
+  mouse = createVector(mouseX, mouseY);
+}
+
+//Classes
+class Hand {
+  constructor(){
+    this.position = createVector(mouseX, mouseY);
   }
+  update(){
+    this.position = createVector(mouseX, mouseY);
+  }
+  draw(){
+    ellipse(this.position.x, this.position.y, 25);
+  }
+}
+
+//class creations
+let newHand = new Hand();
+
+//Draw function
+function draw() {
+  background(100, 100, 100);
+  newHand.update();
+  newHand.draw();
+}
