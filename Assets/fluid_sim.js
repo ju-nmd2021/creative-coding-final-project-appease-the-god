4512350ffde6f2b110ab28983155a2d8f422bdee
Saika0k1 <phopat91@gmail.com>
2023-10-11 23:44:58 +0200

    meow :3

--- Assets/fluid_sim.js
+++ Assets/fluid_sim.js
@@ -1,112 +1,254 @@
-const N = 25;
-const size = 20;
-const h = N * size;
-const dt = 0.2;
-
-let x = mouseX;
-let y = mouseY;
-let x0 = pmouseX;
-let y0 = pmouseY;
-
-let v = createVector(x, y);
-let v0 = createVector(x0, y0);
-let d = new Array((N+2) * (N+2));
-let d0 = new Array((N+2) * (N+2));
-
-function IX(x, y) {
-    return x + y * (N+2);
+function wrap(x, m){
+    return (x + m) % m;
 }
 
-function addSource(N, d, d0, dt) {
-    let size = (N+2) * (N+2);
-
-    for (let i = 0; i < size; i++) {
-        d[i] += dt * d0[i];
-    }
-}
-
-function diffuse(N, b, x, x0, diff, dt) {
-    let a = dt * diff * N * N;
-
-    for (let k = 0; k < 20; k++) {
-        for (let i = 0; i <= N; i ++) {
-            for (let j = 0; j <= N; i++) {
-                x[IX(i,j)] = (x0[IX(i,j)] + a*(x[IX(i-1,j)] + x[IX(i+1,j)] + x[IX(i,j-1)] + x[IX(i,j+1)])) / (1 + 4*a);
-            }
-        }
-    }
-
-    set_bnd(N, b, x); 
-}
-
-function advect(N, b, d, d0, v, dt) {
-    let x;
-    let y;
-    let i;
-    let i0;
-    let i1;
-    let j;
-    let j0;
-    let j1;
-    let s0; 
-    let s1; 
-    let t0;
-    let t1; 
-    let dt0 = dt * N;
-
-    for (i = 0; i <= N; i ++) {
-        for (j = 0; j <= N; i++) {
-            x = i - dt0 * v.x;
-            y = j - dt0 * v.y;
-
-            if (x < 0.5) x = 0.5;
-            if (y < 0.5) y = 0.5;
-            if (x > N + 0.5) x = N + 0.5;
-            if (y > N + 0.5) y = N + 0.5;
-
-            i0 = Math.floor(x);
-            i1 = i0 + 1;
-            j0 = Math.floor(y);
-            j1 = j0 + 1;
-
-            s1 = x - i0;
-            s0 = 1 - s1;
-            
-            t1 = x - j0;
-            t0 = 1 - t1;
-
-            d[IX(i,j)] = s0 * (t0*d0[IX(i0,j0)] + t1*d0[IX(i0,j1)]) + s1 * (t0*d0[IX(i1,j0)] + t1*d0[IX(i1,j1)]);
-        }
-    }
-
-    set_bnd(N, b, d);
-}
-
-function densStep (N , x, x0, v, diff, dt) {
-    let tmp_;
-    let tmp;
-
-    addSource(N, x, x0, dt);
-    
-    tmp_ = x0;
-    x0 = x;
-    x = tmp;
-
-    diffuse(N, 0, x, x0, diff, dt);
-    advect(N, 0, x, x0, v.x, v.y, dt);
-}
-
-function setup() {
-    createCanvas(h, h);
-    background(255);
+function arrow(pos, dir){
+    line(pos.x, pos.y, pos.x + dir.x, pos.y + dir.y);
     push();
-    stroke(0, 100);
-    for (let i = 0; i < N; i++) {
-        line(i * size, 0, i * size, h);
-        line(0, i * size, h, i * size);
-    }
+    translate(pos.x + dir.x, pos.y + dir.y);
+    rotate(dir.heading() - HALF_PI);
+    triangle(-3,-2, 3,-2, 0,4);
     pop();
 }
 
+class Fluid {
+    constructor(w, h, size, k, nu){
+        this.w = w;
+        this.h = h;
+        this.size = size;
+        this.k = k;
+        this.nu = nu;
+        
+        this.density = [];
+        this.velocity = [];
+        for(let i = 0; i < this.w; ++i){
+            this.density.push([]);
+            this.velocity.push([]);
+            for(let j = 0; j < this.h; ++j){
+                this.density[i].push(0);
+                /*let u = createVector(i - this.w/2, j - this.h/2);
+                let a = u.heading() + HALF_PI;
+                let v = createVector(cos(a), sin(a));
+                v.mult(u.mag() * 10);*/
+                this.velocity[i].push(createVector(0,0));
+            }
+        }
+    }
+    
+    simulate(dt){
+            let xs = [];
+            let ys = [];
+            for(let i = 0; i < this.w; ++i){
+                xs.push([]);
+                ys.push([]);
+                for(let j = 0; j < this.h; ++j){
+                xs[i].push(this.velocity[i][j].x);
+                ys[i].push(this.velocity[i][j].y);
+                }
+        }
+        
+        this.diffuse(xs, this.nu, dt);
+        this.diffuse(ys, this.nu, dt);
+        
+        for(let i = 0; i < this.w; ++i){
+            for(let j = 0; j < this.h; ++j){
+                this.velocity[i][j] = createVector(xs[i][j], ys[i][j]);
+            }
+        }
+        
+        this.clearDivergence();
+        
+        for(let i = 0; i < this.w; ++i){
+            for(let j = 0; j < this.h; ++j){
+                xs[i][j] = this.velocity[i][j].x;
+                ys[i][j] = this.velocity[i][j].y;
+            }
+        }
+        
+        this.advect(xs, dt);
+        this.advect(ys, dt);
+        
+        for(let i = 0; i < this.w; ++i){
+            for(let j = 0; j < this.h; ++j){
+                this.velocity[i][j] = createVector(xs[i][j], ys[i][j]);
+            }
+        }
+        
+        this.clearDivergence();
+        
+        this.diffuse(this.density, this.k, dt);
+        this.advect(this.density, dt);
+    }
+        
+    diffuse(grid, coef, dt){
+        let a = dt * coef;
+        
+        for(let k = 0; k < 10; ++k){
+            for(let i = 0; i < this.w; ++i){
+                for(let j = 0; j < this.h; ++j){
+                    
+                    let top     = grid[i][wrap(j + 1, this.h)];
+                    let bottom  = grid[i][wrap(j - 1, this.h)];
+                    let left    = grid[wrap(i - 1, this.w)][j];
+                    let right   = grid[wrap(i + 1, this.w)][j];
+                    
+                    let curVal = grid[i][j];
+                    
+                    let newVal = curVal + a * (top + bottom + left + right);
+                    newVal /= 4 * a + 1;
+                    
+                    grid[i][j] = newVal;
+                }
+            }
+        }
+    }
+    
+    advect(grid, dt) {
+        for(let i = 0; i < this.w; ++i){
+            for(let j = 0; j < this.h; ++j){
+            let v = this.velocity[i][j].copy();
+            let x = i * this.size;// + this.size/2;
+            let y = j * this.size;// + this.size/2;
+            let f = createVector(x, y);
+            v.mult(dt);
+            f.sub(v);
+            f.mult(1 / this.size);
+            f = createVector(wrap(f.x, this.w), wrap(f.y, this.h));
+            let fi = createVector(floor(f.x), floor(f.y));
+            let fj = createVector(fract(f.x), fract(f.y));
+            
+            let z1 = lerp(grid[fi.x][fi.y],
+                            grid[wrap(fi.x + 1, this.w)][fi.y],
+                            fj.x);
+            let z2 = lerp(grid[fi.x][wrap(fi.y + 1, this.h)],
+                            grid[wrap(fi.x + 1, this.w)][wrap(fi.y + 1, this.h)],
+                            fj.x);
+            
+            let newVal = lerp(z1, z2, fj.y);
+            
+            grid[i][j] = newVal;
+            }
+        }
+    }
+    
+    clearDivergence(){
+        let div = [];
+        let p = [];
+        //let h = 1/this.w;
+        
+        for(let i = 0; i < this.w; ++i){
+            div.push([]);
+            p.push([]);
+            for(let j = 0; j < this.h; ++j){
+            let d = this.velocity[wrap(i + 1, this.w)][j].x - 
+                    this.velocity[wrap(i - 1, this.w)][j].x +
+                    this.velocity[i][wrap(j + 1, this.h)].y - 
+                    this.velocity[i][wrap(j - 1, this.h)].y;
+            d /= 2;
+            div[i].push(d);
+            p[i].push(0);
+            }
+        }
+        
+        for(let k = 0; k < 20; ++k) {
+            for(let i = 0; i < this.w; ++i){
+                for(let j = 0; j < this.h; ++j){
+                    p[i][j] = -div[i][j] +
+                            p[wrap(i - 1, this.w)][j] + p[wrap(i + 1, this.w)][j] +
+                            p[i][wrap(j - 1, this.h)] + p[i][wrap(j + 1, this.h)];
+                    p[i][j] /= 4;
+                }
+            }
+        }
+        
+        for(let i = 0; i < this.w; ++i){
+            for(let j = 0; j < this.h; ++j){
+                this.velocity[i][j].x -= 0.5 * (p[wrap(i + 1, this.w)][j] -
+                                                p[wrap(i - 1, this.w)][j]);
+                this.velocity[i][j].y -= 0.5 * (p[i][wrap(j + 1, this.h)] -
+                                                p[i][wrap(j - 1, this.h)]);
+            }
+        }
+    }
+    
+    show(showVel){
+        noStroke();
+        noFill();
+        for(let i = 0; i < this.w; ++i){
+            for(let j = 0; j < this.h; ++j){
+                let x = i * this.size;
+                let y = j * this.size;
+                fill(255 * this.density[i][j]);
+                rect(x, y, x + this.size, y + this.size);
+            }
+        }
+        
+        if(!showVel) return;
+        
+        fill(255,0,0);
+        stroke(255, 0, 0);
+        for(let i = 0; i < this.w; ++i){
+            for(let j = 0; j < this.h; ++j){
+                let vel = this.velocity[i][j];
+                if(vel.magSq() == 0) continue;
+                let x = i * this.size + this.size/2;
+                let y = j * this.size + this.size/2;
+                arrow(createVector(x, y), vel);
+            }
+        }
+    }
+}
+
+const N = 40;
+const SCALE = 10;
+const DENS_DECAY = 0.05;
+const VESCOCITY = 0.985;
+let fluid;
+let t;
+
+function setup() {
+    createCanvas(N * SCALE, N * SCALE, WEBGL);
+    
+    fluid = new Fluid(N, N, ceil(width / N), 0.05, 0.8);
+    
+    t = 0;
+}
+
 function draw() {
+    background(0);
+    translate(-N * SCALE/2, -N * SCALE/2)
+    
+    let dt = frameRate() > 0 ? 1 / frameRate() : 0;
+    
+    fluid.simulate(dt);
+
+    // let x = floor(fluid.w / 2);
+    // let y = floor(fluid.h / 2);
+    // fluid.density[x][y] = 1;
+    // let nt = noise(t)*TWO_PI*2;
+    // fluid.velocity[x][y].add(createVector(1000*cos(nt),1000*sin(nt)));
+    
+    fluid.show(false);
+    // fluid.show(true);
+    
+    for(let i = 0; i < fluid.w; i++) {
+        for(let j = 0; j < fluid.h; j++){
+            fluid.density[i][j] -= DENS_DECAY * dt;
+            fluid.velocity[i][j].mult(VESCOCITY);
+        }
+    }
+
+    // console.log(frameRate())
+    
+    t += dt;
+}
+
+function mouseDragged() { 
+    let x = constrain(floor(mouseX/SCALE), 0, N-1);
+    let y = constrain(floor(mouseY/SCALE), 0, N-1);
+    
+    let amtX = mouseX - pmouseX;
+    let amtY = mouseY - pmouseY;
+    fluid.density[x][y] = 15;
+    fluid.velocity[x][y].add(createVector(100 * amtX, 100 * amtY));
 }
